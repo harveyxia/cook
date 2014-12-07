ba034fa33116e06149a48a46c1e74317fd18fe54
Harvey Xia <harvey@harveyxia.com>
2014-12-07 16:33:05 -0500

    Added navbutton and key press nav. Start voice nav

--- cook.js
+++ cook.js
@@ -1,58 +1,112 @@
 var App = angular.module('App', []);
 
-App.controller('RecipeCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log('asdf');
+App.controller('RecipeCtrl', ['$rootScope', '$scope', '$http',
+    function($rootScope, $scope, $http) {
     $http.get('sample_recipe.json')
          .then(function(res) {
             $scope.r = res.data;
             $scope.r.ingredients = formatRecipe(res.data);
             $scope.steps = allSteps(res.data.steps);
-            console.log($scope.steps);
          });
     $scope.slideDown = slideDown;
+    $scope.slideUp = slideUp;
     $scope.stepIndex = 0;
     $scope.incrementStep = incrementStep;
     $scope.decrementStep = decrementStep;
-    console.log($scope.stepIndex);
-}]);
-
-function formatRecipe(r) {
-    var formatted_r = {};
-    for (section in r.ingredients) {
-        formatted_r[section] = {};
-        for (item in r.ingredients[section]) {
-            var i = r.ingredients[section][item]
-            if (i.constructor === Array) {
-                i.forEach(function(type) {
-                    var new_name = type.name + ' ' + type.type;
-                    formatted_r[section][new_name] = type;
-                });
-            } else  {
-                formatted_r[section][item] = i;
+    $scope.arrowKeyNav = arrowKeyNav;
+    
+    if (!('webkitSpeechRecognition' in window)) {
+      alert("Upgrade yo shit browser");
+    } else {
+        recognition = new webkitSpeechRecognition();
+        recognition.lang = 'en-US';
+        recognition.continuous = true;
+        recognition.onstart = function(e) {
+            console.log('starting capture');
+        }
+        console.log($scope.stepIndex);
+        recognition.onresult = function(e) {
+            results = e.results[0];
+            var latestEvent = event.results[event.results.length-1][0].transcript;
+            latestEvent = $.trim(latestEvent);
+            console.log($.trim(latestEvent));
+            if (latestEvent === "back") {
+                console.log('back');
+                return decrementStep($scope.stepIndex);
+            } else if (latestEvent === "next") {
+                console.log('next');
+                console.log(incrementStep($scope.stepIndex, $scope.steps.length));
+                return incrementStep($scope.stepIndex, $scope.steps.length);
             }
         }
-    };
-    return formatted_r;
-}
+        $scope.stepIndex = recognition.onresult;
+    }
 
-function allSteps(steps) {
-    var allSteps = [];
-    steps.forEach(function(step) {
-        allSteps = allSteps.concat(step.procedure);
-    });
-    return allSteps;
-}
+    // $scope.stepIndex = recognition.onresult;
 
-function incrementStep(i, steps) {
-    console.log(i);
-    return Math.min(i+1, steps-1);
-}
+    function formatRecipe(r) {
+        var formatted_r = {};
+        for (section in r.ingredients) {
+            formatted_r[section] = {};
+            for (item in r.ingredients[section]) {
+                var i = r.ingredients[section][item]
+                if (i.constructor === Array) {
+                    i.forEach(function(type) {
+                        var new_name = type.name + ' ' + type.type;
+                        formatted_r[section][new_name] = type;
+                    });
+                } else  {
+                    formatted_r[section][item] = i;
+                }
+            }
+        };
+        return formatted_r;
+    }
 
-function decrementStep(i) {
-    return Math.max(i-1, 0);
-}
+    function allSteps(steps) {
+        var allSteps = [];
+        steps.forEach(function(step) {
+            allSteps = allSteps.concat(step.procedure);
+        });
+        return allSteps;
+    }
 
-function slideDown() {
-    console.log($('.overlay'));
-    $('.overlay').css('top', 0);
-}
+    function incrementStep(i, steps) {
+        $scope.stepIndex = Math.min(i+1, steps-1);
+        // return Math.min(i+1, steps-1);
+    }
+
+    function decrementStep(i) {
+        $scope.stepIndex = Math.max(i-1, 0);
+        // return Math.max(i-1, 0);
+    }
+
+    function arrowKeyNav(e, stepIndex, steps) {
+        switch(e.keyCode) {
+            case 37:
+                return decrementStep(stepIndex);
+            case 39:
+                return incrementStep(stepIndex, steps);
+            case 27:
+                slideUp();
+                return stepIndex;
+            default:
+                return stepIndex;
+        }
+    }
+
+    function slideDown() {
+        $('.overlay').css('top', 0);
+        $('html').css('overflow-y', 'hidden');
+        $('.overlay').focus();
+
+        recognition.start();
+    }
+
+    function slideUp() {
+        $('.overlay').css('top', '-100%');
+        $('html').css('overflow-y', 'auto');
+
+        recognition.stop();
+    }
+}]);
